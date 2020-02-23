dnaeon/pytraceroute

commit 1cac873aa9e864314871b033cd8c59c2f733e20a

add timer

src/traceroute/core.py
@@ -30,6 +30,7 @@ Core module
 import socket
 import random
 import struct
+import time
 
 __all__ = ['Tracer']
 
@@ -73,6 +74,7 @@ class Tracer(object):
         print(text)
 
         while True:
+            startTimer = time.time()
             receiver = self.create_receiver()
             sender = self.create_sender()
             sender.sendto(b'', (self.dst, self.port))
@@ -80,6 +82,7 @@ class Tracer(object):
             addr = None
             try:
                 data, addr = receiver.recvfrom(1024)
+                entTimer = time.time()
             except socket.error:
                 pass
                 # raise IOError('Socket error: {}'.format(e))
@@ -88,7 +91,8 @@ class Tracer(object):
                 sender.close()
 
             if addr:
-                print('{:<4} {}'.format(self.ttl, addr[0]))
+                timeCost = round((entTimer - startTimer) * 1000, 2)
+                print('{:<4} {} {} ms'.format(self.ttl, addr[0]), timeCost)
                 if addr[0] == dst_ip:
                     break
             else:

commit 38873b461a3f3013e02708eb7cfb5c37ec84b84e

fix crash from bogus output str .format
Bogus str .format (wrong number of args) crashes the library upon
reporting the first received packet:

        print('{:<4} {} {} ms'.format(self.ttl, addr[0]), timeCost)
    IndexError: tuple index out of range

Fix it by moving timeCost into the .format call where it belongs.

Fixes: 1cac873 ("add timer")

src/traceroute/core.py
@@ -92,7 +92,7 @@ class Tracer(object):
 
             if addr:
                 timeCost = round((entTimer - startTimer) * 1000, 2)
-                print('{:<4} {} {} ms'.format(self.ttl, addr[0]), timeCost)
+                print('{:<4} {} {} ms'.format(self.ttl, addr[0], timeCost))
                 if addr[0] == dst_ip:
                     break
             else: